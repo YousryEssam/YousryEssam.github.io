8cd8554ca2de5429ae59b6baab8ffd323b4d44e5
YousryEssam <youssry.essam2111@gmail.com>
2024-12-04 23:22:48 +0200

    General-Enhancement V2.4

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # YousryEssam.github.io
+
 ## Welcome to My portfolio
 
 This is a web-based version of my resume, created using HTML , CSS and JavaScript. Hosted on GitHub Pages. You can visit the live website by following the link below:
@@ -8,6 +9,7 @@ This is a web-based version of my resume, created using HTML , CSS and JavaScrip
 ## About Me
 
 ### Software Developer | Competitive Programmer | ITI Trainee @ PDW & CRM Track
+
 Fresh Computer Science graduate from Assiut University with strong expertise in programming languages such as C, C++, Java, Python, Go, and C#. Accomplished competitive programmer with high rankings in coding contests and active profiles on Codeforces, LeetCode, and HackerRank. Experienced in projects. Currently pursuing the Professional Development & BI-infused CRM training program at ITI's Assiut branch, enhancing technical and professional skills. Passionate about software development, mentoring, and contributing to open-source projects. Some of my notable achievements include:
 
 - **Graduation Project:** ShahraZad (A+ Grade) - An AI-driven audiobook system.
@@ -16,20 +18,23 @@ Fresh Computer Science graduate from Assiut University with strong expertise in 
 - **Volunteering:** C++ instructor and mentor for problem-solving skills.
 
 ## Skills
-- **Basics:** Object-Oriented Programming, problem-solving Skills , Data Structures, Algorithms, Testing & Debugging ,MySQL 
-- **Programming Languages:** C, C++, C# , Java, Python, Go , JavaScript 
-- **Database:** 
+
+- **Basics:** Object-Oriented Programming, problem-solving Skills , Data Structures, Algorithms, Testing & Debugging ,MySQL
+- **Programming Languages:** C, C++, C# , Java, Python, Go , JavaScript
+- **Database:**
 - **Tools:** GIT, HTML , CSS
 
 ## Projects
+
 Here are some of the projects I’ve worked on:
+
 - [ShahraZad - AI Audiobook System](https://github.com/YousryEssam/ShahraZad)
 - [Distributed Simple ATM System](https://github.com/YousryEssam/Distributed-System-Using-Golang)
 - [Devices Price Classification System](https://github.com/YousryEssam/Devices-Price-Classification-System)
 - [Endless Runner Game](https://github.com/YousryEssam/Endless-runner-game-with-unity)
 - [Snake Game](https://github.com/YousryEssam/Snake-Game)
 
-
 ## Contact Me
+
 - **LinkedIn:** [Yousry Essam](https://www.linkedin.com/in/yousryessam)
 - **Email:** [yousry.essam.ayoub@gmail.com](mailto:youssry.essam2111@gmail.com)
--- Script.js
+++ Script.js
@@ -2,52 +2,52 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
 
-
 window.addEventListener("scroll", () => {
-    let currentSection = "home";
+  let currentSection = "home";
 
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
 
-        if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-            currentSection = section.getAttribute("id");
-        }
-    });
+    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute("id");
+    }
+  });
 
-    navLinks.forEach((link) => {
-        link.classList.remove("active");
-        if (link.getAttribute("href").includes(currentSection)) {
-            link.classList.add("active");
-        }
-    });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(currentSection)) {
+      link.classList.add("active");
+    }
+  });
 });
 
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.forEach(nav => nav.classList.remove("active"));
-        link.classList.add("active");
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((nav) => nav.classList.remove("active"));
+    link.classList.add("active");
+  });
 });
 
 // Function to update the progress bar
-window.onscroll = function() {
-var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-var scrolled = (winScroll / height) * 100;
-document.getElementById("progress-bar").style.width = scrolled + "%";
+window.onscroll = function () {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
 };
 
 // Dark - Light mode switch
 var ITI_Logo_path = ["Images/ITI-Logo-Dark.png", "Images/ITI-Logo.svg"];
-function darkModeSwitch(){
-    var darkSwitch = document.getElementById("mainBody");
-    darkSwitch.classList.toggle("darkLightSwitch");
-    var ITI_Logo = document.getElementById("ITI-Logo");
-    if(ITI_Logo.getAttribute("src") == ITI_Logo_path[0]){
-        ITI_Logo.setAttribute("src" ,ITI_Logo_path[1] );
-    }
-    else {
-        ITI_Logo.setAttribute("src" ,ITI_Logo_path[0] );
-    }
+function darkModeSwitch() {
+  var darkSwitch = document.getElementById("mainBody");
+  darkSwitch.classList.toggle("darkLightSwitch");
+  var ITI_Logo = document.getElementById("ITI-Logo");
+  if (ITI_Logo.getAttribute("src") == ITI_Logo_path[0]) {
+    ITI_Logo.setAttribute("src", ITI_Logo_path[1]);
+  } else {
+    ITI_Logo.setAttribute("src", ITI_Logo_path[0]);
+  }
 }
